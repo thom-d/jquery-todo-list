9c601da285c583cdc7248b7eba256e3051548019
dthomas7 <danathomas@quickenloans.com>
2016-07-24 02:26:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// 1. User adds input to form field
+// 2. Add to list is entered to add items to list
+// 3. Add checkbox
+
+$("#list-items").html(localStorage.getItem("listItems"));
+
+$(".add-items").submit(function (event) {
+
+		event.preventDefault();
+
+		var item = $("#todo-list-item").val();
+
+		if (item) {
+			$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+			localStorage.setItem("listItems", $("#list-items").html());
+			$("#todo-list-item").val("");
+		}
+
+});
+
+$(document).on("change", ".checkbox", function() {
+	if ($(this).attr("checked")) {
+		$(this).removeAttr("checked");
+	} else { 
+		$(this).attr("checked", "checked");
+	}
+
+	$(this).parent().toggleClass("completed");
+	localStorage.setItem("listItems", $("#list-items").html());
+});
+
+$(document).on("click", ".remove", function() {
+	$(this).parent().remove();
+	localStorage.setItem("listItems", $("#list-items").html());
+});
